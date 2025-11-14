179c8c12156849e72ad755a7295ce90dfaa6f82f
Hudson <hudson.henri10@gmail.com>
2025-11-14 16:18:42 -0300

    feat(pandas): Adiciona exercícios de manipulação e análise de dados
    Este commit introduz uma série de scripts e exercícios práticos que exploram funcionalidades essenciais da biblioteca Pandas para data wrangling e análise.
    
    As principais técnicas abordadas são:
    - **Criação de Novas Colunas (Feature Engineering):** Geração de novas features a partir de colunas existentes, utilizando operações aritméticas e lógicas.
    - **Tratamento de Dados:** Conversão de tipos (incluindo pd.to_datetime), tratamento de valores ausentes (dropna, illna) e remoção de duplicatas (drop_duplicates).
    - **Filtragem e Seleção:** Uso de filtros booleanos para selecionar subconjuntos de dados e métodos como loc/iloc para acesso a dados específicos.
    - **Análise Exploratória:** Realização de agregações simples, como encontrar o cliente com maior e menor saldo de pontos, e identificar a primeira e última transação.

---
+++ exercicios/04/drop_duplicates.py
@@ -0,0 +1,20 @@
+# %%
+
+# Obtenha a última linha de transacao de cada cliente
+# Obtenha a primeira
+
+import pandas as pd
+
+df = pd.read_csv("../../data/transacoes.csv", sep=";")
+df.head()
+
+# %%
+
+# ultima
+ultima_transacao = (df.sort_values(by="dtCriacao")
+                      .drop_duplicates(subset=['idCliente'], keep='last'))
+
+# %%
+primeira_transacao = (df.sort_values(by="dtCriacao")
+                        .drop_duplicates(subset=['idCliente'], keep='first'))
+# %%
